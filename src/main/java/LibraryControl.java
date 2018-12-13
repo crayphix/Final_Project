3e6fc74970d8b75beb6474238dd4345a74635e92
Bryan Speelman <bryspeelm@gmail.com>
2018-12-12 22:42:15 -0500

    Rough out of Library Control

--- src/main/java/LibraryControl.java
+++ src/main/java/LibraryControl.java
@@ -1,10 +1,3 @@
-import javafx.application.Application;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.*;
-import javafx.scene.text.Text;
-import javafx.stage.Stage;
-
 /*
  * Class: CMSC214
  * Instructor: Alla Webb
@@ -16,17 +9,29 @@ import javafx.stage.Stage;
    I have not given my code to any student.
    Print your Name here: Bryan Speelman
 */
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.*;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
 public class LibraryControl extends Application {
     //Text
     private Text title = new Text("Library Controls"); // Title
+
     //Buttons
-    private Button btConnect = new Button("Connect");
+    private Button btConnect = new Button("Connect To Database");
     private Button btSearchISBN = new Button("Search by ISBN");
-    private Button btSearchUsr = new Button("Search by User");
+    private Button btSearchUsr = new Button("Search by UserID");
     private Button btCheckout = new Button("Checkout");
     private Button btReturn = new Button("Return");
     private Button btRntlCkUsr = new Button("Rental Check (User ID Only)");
     private Button btRnlCkBk = new Button("Check Status(Book ID only)");
+
     //TextBoxes
     private TextField isbn = new TextField(); //ISBN
     private TextField author = new TextField(); //Author
@@ -35,43 +40,105 @@ public class LibraryControl extends Application {
     private TextField usrId = new TextField(); // User ID
     private TextField name = new TextField(); // Name
     private TextField address = new TextField(); // Address
-    private TextField usrid2 = new TextField(); // User ID 2
-    private TextField bookId = new TextField(); // Book ID
+    private TextField usrIdCk = new TextField(); // User ID 2
+    private TextField bookIdCk = new TextField(); // Book ID
     private TextField bookTitle = new TextField(); // Title
 
+    //Vbox
+    private VBox vBox = new VBox(); //VBox build main pane
+
     //HBoxes
+    private HBox titleHBox = new HBox(); //HBox to hold title
+    private HBox menuHBox = new HBox(); //HBox to hold grid pane of menus
+
     //Gridpanes
-    //Panes
-    Pane pane = new Pane();
+    private GridPane bookPane = new GridPane(); //Grid Pane to hold book info
+    private GridPane userPane = new GridPane(); //Grid Pane to hold user info
+    private GridPane checkPane = new GridPane(); //Grid pane to hold checkout status
+
+    //TextArea
+    private TextArea status = new TextArea(); //Text Area to hold status of querys
 
     @Override
     public void start(Stage primaryStage) throws Exception {
         //Main Pane
-            //Grid Pane
-                //(0,0) (3, 0) HBox to hold Title and connection button
-                    //Button to Connect to Database
-                    //Add children to hbox
-                    //Add HBox to gridpane
-                //(0, 1) GridPane to hold Book Info
-                //Textbox w/ Label ISBN
+        Pane pane = new Pane();
 
-        //Textboax w/ Label Title
-        //TextBox w/ label Author
-        //TextBox w/ label Book ID's CheckedIn
-        //TextBox w/ label Book Id's Checkedout
-        //Button to search by isbn
-        //(1, 1) VBox to hold User info
-        //Textbox w/ label User Id
-        //Textbox w/ label Name
-        //TextBox w/ label Address
-        //Button to search by userId
-        //(2, 1) VBox to hold Book status and activation
-        //TextBox w/ label User Id
-        //TextBox w/ label Book Id
-        //Button To checkout Book
-        //Button to return book
-        //Button to check rentals by User ID
-        //Button to check status by Book Id
-        //(0, 2) TextArea(Scrollable) for giving detailed information by line
+        //Vbox
+        VBox vBox = new VBox();
+
+        //Set title
+        title.setText("Library Controls");
+        title.setStyle("-fx-font-size: 20");
+        titleHBox.getChildren().addAll(title, btConnect);
+        titleHBox.setPadding(new Insets(5,5,5,5));
+        titleHBox.setSpacing(550);
+
+        //build book menu pane
+        bookPane.add(new Text("ISBN: "), 0, 0);
+        bookPane.add(isbn, 1, 0);
+        bookPane.add(new Text("Title: "),0,2);
+        bookPane.add(bookTitle, 1, 2);
+        bookTitle.setEditable(false);
+        bookPane.add(new Text("Author: "),0, 3);
+        bookPane.add(author, 1,3);
+        author.setEditable(false);
+        bookPane.add(new Text("Book IDs in: "), 0, 4);
+        bookPane.add(bookIdIn, 1, 4);
+        bookIdIn.setEditable(false);
+        bookIdIn.setMinHeight(70);
+        bookPane.add(new Text("Book IDs out"), 0, 5);
+        bookPane.add(bookIdOut, 1, 5);
+        bookIdOut.setEditable(false);
+        bookIdOut.setMinHeight(70);
+        bookPane.add(btSearchISBN,0,6);
+        bookPane.setStyle("-fx-border-color: BLACK; -fx-spacing: 10");
+        bookPane.setVgap(5);
+
+        //build user menu
+        userPane.add(new Text("User ID: "), 0, 0);
+        userPane.add(usrId, 1,0);
+        userPane.add(new Text("Name: "), 0, 1);
+        userPane.add(name, 1, 1);
+        name.setEditable(false);
+        userPane.add(new Text("Address: "), 0, 2);
+        userPane.add(address,1,2);
+        address.setEditable(false);
+        address.setMinHeight(70);
+        userPane.add(btSearchUsr,0,3);
+        userPane.setStyle("-fx-border-color: BLACK; -fx-spacing: 10");
+        userPane.setVgap(5);
+
+        //build checkout menu
+        checkPane.add(new Text("User ID: "),0,0);
+        checkPane.add(usrIdCk, 1,0);
+        checkPane.add(new Text("Book ID: "),0,1);
+        checkPane.add(bookIdCk, 1,1);
+        checkPane.add(new Text("Check Out Book"),0 ,2);
+        checkPane.add(btCheckout,1,2);
+        checkPane.add(new Text("Return Book"), 0, 3);
+        checkPane.add(btReturn, 1, 3);
+        checkPane.add(new Text("Check Rentals By User ID"), 0, 4);
+        checkPane.add(btRntlCkUsr, 1, 4);
+        checkPane.add(new Text("Check Rental by Book ID"), 0, 5);
+        checkPane.add(btRnlCkBk, 1, 5);
+        checkPane.setStyle("-fx-border-color: BLACK; -fx-spacing: 10");
+        checkPane.setVgap(5);
+
+        //Build main pane
+        status.setMinSize(700, 150);
+        status.setEditable(false);
+        status.setScrollTop(0);
+        menuHBox.setSpacing(10);
+        menuHBox.getChildren().addAll(bookPane, userPane, checkPane);
+        vBox.getChildren().addAll(titleHBox, menuHBox, status);
+        pane.getChildren().addAll(vBox);
+
+
+        // Create a scene and place the pane in the stage
+        Scene scene = new Scene(pane, 825, 450);
+        primaryStage.setTitle("Library Software"); // Set the stage title
+        primaryStage.setScene(scene); // Place the scene in the stage
+        primaryStage.show(); // Display the stage
     }
 }
--- src/main/resources/DBScript.sql
+++ src/main/resources/DBScript.sql
@@ -1,38 +1,42 @@
 use bookslibrary;
+
 drop table if exists books;
 drop table if exists users;
 create table books (
   bookId char(4) not null, 
-  isbn varchar(25) unique, 
+  isbn varchar(25) ,
   title varchar(50), 
   Author varchar(35),
-  checkout boolean; 
-  constraint pkbooks primary key (bookId));
+  checkout boolean,
+constraint pkbooks primary key (bookId));
+
 create table users (
   userId char(4) not null, 
   name char(35) not null,
   address varchar(50) not null,
   constraint pkusers primary key (userId));
+
 insert into books values (
-  '1', '0133761312', 'Intro to Java Programming (10e)', 'Y. Daniel Liang', 'F');
+  '1', '0133761312', 'Intro to Java Programming (10e)', 'Y. Daniel Liang', '0');
 insert into books values (
-  '2', '0133761312', 'Intro to Java Programming (10e)', 'Y. Daniel Liang', 'F');
+  '2', '0133761312', 'Intro to Java Programming (10e)', 'Y. Daniel Liang', '0');
 insert into books values (
-  '3', '0133761312', 'Intro to Java Programming (10e)', 'Y. Daniel Liang', 'F');
+  '3', '0133761312', 'Intro to Java Programming (10e)', 'Y. Daniel Liang', '0');
 insert into books values (
-  '4', '0062320130', 'The English Spy', 'Daniel Silva', 'F');
+  '4', '0062320130', 'The English Spy', 'Daniel Silva', '0');
 insert into books values (
-  '5', '0062320130', 'The English Spy', 'Daniel Silva', 'F');
+  '5', '0062320130', 'The English Spy', 'Daniel Silva', '0');
 insert into books values (
-  '6', '0062320130', 'The English Spy', 'Daniel Silva', 'F');
+  '6', '0062320130', 'The English Spy', 'Daniel Silva', '0');
 insert into books values (
-  '7', '0345534182', 'Circling the Sun', 'Deckle Edge', 'F');
+  '7', '0345534182', 'Circling the Sun', 'Deckle Edge', '0');
 insert into books values (
-  '8', '0345534182', 'Circling the Sun', 'Deckle Edge', 'F');
+  '8', '0345534182', 'Circling the Sun', 'Deckle Edge', '0');
 insert into books values (
-  '9', '1627792120', 'Six of Crows', 'Leigh Bardugo', 'F');
+  '9', '1627792120', 'Six of Crows', 'Leigh Bardugo', '0');
 insert into books values (
-  '10', '1627792120', 'Six of Crows', 'Leigh Bardugo', 'F');
+  '10', '1627792120', 'Six of Crows', 'Leigh Bardugo', '0');
+
 insert into users values (
 '1', 'John Smith', '3240 Walnut Avenue');
 insert into users values (
